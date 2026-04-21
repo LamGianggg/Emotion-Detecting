ade401519351b6f6d591ceabf82849ac8d9adf0b
wew57 <huy1872006k@gmail.com>
2026-04-21 09:58:40 +0700

    add file

---
+++ training/t1.py
@@ -0,0 +1,15 @@
+import torchaudio
+import torch
+import matplotlib
+import numpy
+import scipy
+import pandas
+import sklearn
+
+print(torchaudio.__version__)
+print(torch.__version__)
+print(matplotlib.__version__)
+print(numpy.__version__)
+print(scipy.__version__)
+print(pandas.__version__)
+print(sklearn.__version__)
